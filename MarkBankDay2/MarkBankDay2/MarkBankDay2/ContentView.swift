332a14e257441453d8e875a28e3f27863770fd4a
Priya Sadhale <priyasadhale@live.com>
2022-10-13 14:35:02 +0800

    MarkBank - Day 2

---
+++ MarkBankDay2/MarkBankDay2/MarkBankDay2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MarkBankDay2
+//
+//  Created by user220156 on 9/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            
+            .padding()
+           
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarkBankDay2/MarkBankDay2/MarkBankDay2/MarkBankDay2App.swift
@@ -0,0 +1,17 @@
+//
+//  MarkBankDay2App.swift
+//  MarkBankDay2
+//
+//  Created by user220156 on 9/8/22.
+//
+
+import SwiftUI
+
+@main
+struct MarkBankDay2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
